0064671885a096fc5546bbf443516feb8319d3c1
Keshawn <strongkeshawn047@gmail.com>
2025-03-28 10:31:25 -0700

    Test changePrice(); subscriptionData visibilty changed to public; remove .price from memberships

--- test/test_subscriptions.js
+++ test/test_subscriptions.js
@@ -15,11 +15,11 @@ contract("Subscriptions", (accounts) => {
         rootInstance = await Subscription.new(_name, _price, _timeframe);
     })
 
-    it("Test: If subscription was created sucessfully", async () => {
+    it("Test: subscribe(), If subscription was created sucessfully", async () => {
 
         const amtOfSubs = await rootInstance.getSubscribers.call();
         const amtOfCancellations = await rootInstance.getCancellations.call(); 
-        const getSubscriptionStruct = await rootInstance.getSubscriptionInfo.call(); 
+        const getSubscriptionStruct = await rootInstance.subscriptionData.call(); 
         const name = getSubscriptionStruct.name; 
         const price = getSubscriptionStruct.price; 
 
@@ -30,24 +30,24 @@ contract("Subscriptions", (accounts) => {
     });
 
     
-    it("Test: If the user was able to subscribe", async () => {
+    it("Test: subscribe(), If the user was able to subscribe", async () => {
         await rootInstance.subscribe({from: mainHolderAcct}); 
         const getSubscribers = await rootInstance.getSubscribers.call(); 
 
         assert.equal(getSubscribers, 1); 
     }); 
 
-    it("Test: If they're subscribed to the subscription", async() => {
+    it("Test: subscribe(), If they're subscribed to the subscription", async() => {
         const isSubscribed = await rootInstance._subscribed(mainHolderAcct); 
         assert.equal(isSubscribed, true);
     });
 
-    it("Test: If newly subscribed account became the main account holder", async() => {
+    it("Test: subscribe(), If newly subscribed account became the main account holder", async() => {
         const acctHolder = await rootInstance.subscription(mainHolderAcct);
         assert.equal(acctHolder.mainAcct, mainHolderAcct);
     }); 
 
-    it("Test: Should not allow user with an account to subscribe again", async() => {
+    it("Test: subscribe(), Should not allow user with an account to subscribe again", async() => {
         const errMsg = "You already have a subscription"; 
         const revertString = `VM Exception while processing transaction: revert ${errMsg} -- Reason given: ${errMsg}.`; 
 
@@ -61,7 +61,7 @@ contract("Subscriptions", (accounts) => {
         }
     });
 
-    it("Test: Should not allow owner of contract to subscribe to their own subscription", async() => {
+    it("Test: subscribe(), Should not allow owner of contract to subscribe to their own subscription", async() => {
         const errMsg = "You cannot perform this action"; 
         const revertString = `VM Exception while processing transaction: revert ${errMsg} -- Reason given: ${errMsg}.`; 
 
@@ -75,20 +75,33 @@ contract("Subscriptions", (accounts) => {
         }
     });
 
-    it("Test: Membership price should be eq to the subscription price", async() => {
-        const getMembershipPrice = await rootInstance.subscription(mainHolderAcct); 
-        const getSubscriptionPrice = await rootInstance.getSubscriptionInfo(); 
-        assert.equal(getSubscriptionPrice.price, getMembershipPrice.price); 
-    }); 
-
-    it("Test: Should cancel main holder's membership", async() => {
+    it("Test: cancel(), Should cancel main holder's membership", async() => {
         await rootInstance.cancel({from: mainHolderAcct}); 
         const isSubscribed = await rootInstance._subscribed(mainHolderAcct); 
         assert.equal(isSubscribed, false); 
     }); 
 
-    it("Test: Set ownedPreviousMembership flag to true after canceling", async() => {
+    it("Test: cancel(), Set ownedPreviousMembership flag to true after canceling", async() => {
         const membership = await rootInstance.subscription(mainHolderAcct);
         assert.equal(membership.ownedPreviousMembership, true); 
     });
+
+    it("Test: changePrice(), Should change price of subscription", async() => {
+        await rootInstance.changePrice(200); 
+        const getSubscriptionData = await rootInstance.subscriptionData(); 
+        assert.equal(getSubscriptionData.price.words[0], 200);
+    }); 
+
+    it("Test: changePrice(), Should revert if not owner of the contract", async() => {
+        const errMsg = "Custom error (could not decode)"; //Custom error from onlyOwner 
+        const revertString = `VM Exception while processing transaction: revert -- Reason given: ${errMsg}.`; 
+        
+        try{
+            await rootInstance.changePrice(200, {from: mainHolderAcct}); 
+            assert.fail()
+        }
+        catch(error) {
+            assert.equal(error.message, revertString);
+        }
+    })
 })
